3c5795288bd031626c81affb0cf46ef46b99cd64
chrisindris <chrisindris@gmail.com>
2024-02-25 13:12:08 -0500

    configs for i5O/r21d and thumos/tsp

--- libs/utils/train_utils.py
+++ libs/utils/train_utils.py
@@ -38,17 +38,16 @@ def fix_random_seed(seed, include_cuda=True):
     return rng_generator
 
 
-def save_checkpoint(state, is_best, file_folder,
-                    file_name='checkpoint.pth.tar'):
+def save_checkpoint(state, is_best, file_folder, file_name="checkpoint.pth.tar"):
     """save checkpoint to file"""
     if not os.path.exists(file_folder):
         os.mkdir(file_folder)
     torch.save(state, os.path.join(file_folder, file_name))
     if is_best:
         # skip the optimization / scheduler state
-        state.pop('optimizer', None)
-        state.pop('scheduler', None)
-        torch.save(state, os.path.join(file_folder, 'model_best.pth.tar'))
+        state.pop("optimizer", None)
+        state.pop("scheduler", None)
+        torch.save(state, os.path.join(file_folder, "model_best.pth.tar"))
 
 
 def print_model_params(model):
@@ -71,20 +70,20 @@ def make_optimizer(model, optimizer_config):
     # loop over all modules / params
     for mn, m in model.named_modules():
         for pn, p in m.named_parameters():
-            fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
-            if pn.endswith('bias'):
+            fpn = "%s.%s" % (mn, pn) if mn else pn  # full param name
+            if pn.endswith("bias"):
                 # all biases will not be decayed
                 no_decay.add(fpn)
-            elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
+            elif pn.endswith("weight") and isinstance(m, whitelist_weight_modules):
                 # weights of whitelist modules will be weight decayed
                 decay.add(fpn)
-            elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
+            elif pn.endswith("weight") and isinstance(m, blacklist_weight_modules):
                 # weights of blacklist modules will NOT be weight decayed
                 no_decay.add(fpn)
-            elif pn.endswith('scale') and isinstance(m, (Scale, AffineDropPath)):
+            elif pn.endswith("scale") and isinstance(m, (Scale, AffineDropPath)):
                 # corner case of our scale layer
                 no_decay.add(fpn)
-            elif pn.endswith('rel_pe'):
+            elif pn.endswith("rel_pe"):
                 # corner case for relative position encoding
                 no_decay.add(fpn)
 
@@ -92,40 +91,42 @@ def make_optimizer(model, optimizer_config):
     param_dict = {pn: p for pn, p in model.named_parameters()}
     inter_params = decay & no_decay
     union_params = decay | no_decay
-    assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
-    assert len(param_dict.keys() - union_params) == 0, \
-        "parameters %s were not separated into either decay/no_decay set!" \
-        % (str(param_dict.keys() - union_params), )
+    assert (
+        len(inter_params) == 0
+    ), "parameters %s made it into both decay/no_decay sets!" % (str(inter_params),)
+    assert (
+        len(param_dict.keys() - union_params) == 0
+    ), "parameters %s were not separated into either decay/no_decay set!" % (
+        str(param_dict.keys() - union_params),
+    )
 
     # create the pytorch optimizer object
     optim_groups = [
-        {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": optimizer_config['weight_decay']},
-        {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
+        {
+            "params": [param_dict[pn] for pn in sorted(list(decay))],
+            "weight_decay": optimizer_config["weight_decay"],
+        },
+        {
+            "params": [param_dict[pn] for pn in sorted(list(no_decay))],
+            "weight_decay": 0.0,
+        },
     ]
 
     if optimizer_config["type"] == "SGD":
         optimizer = optim.SGD(
             optim_groups,
             lr=optimizer_config["learning_rate"],
-            momentum=optimizer_config["momentum"]
+            momentum=optimizer_config["momentum"],
         )
     elif optimizer_config["type"] == "AdamW":
-        optimizer = optim.AdamW(
-            optim_groups,
-            lr=optimizer_config["learning_rate"]
-        )
+        optimizer = optim.AdamW(optim_groups, lr=optimizer_config["learning_rate"])
     else:
         raise TypeError("Unsupported optimizer!")
 
     return optimizer
 
 
-def make_scheduler(
-    optimizer,
-    optimizer_config,
-    num_iters_per_epoch,
-    last_epoch=-1
-):
+def make_scheduler(optimizer, optimizer_config, num_iters_per_epoch, last_epoch=-1):
     """create scheduler
     return a supported scheduler
     All scheduler returned by this function should step every iteration
@@ -142,21 +143,21 @@ def make_scheduler(
         if optimizer_config["schedule_type"] == "cosine":
             # Cosine
             scheduler = LinearWarmupCosineAnnealingLR(
-                optimizer,
-                warmup_steps,
-                max_steps,
-                last_epoch=last_epoch
+                optimizer, warmup_steps, max_steps, last_epoch=last_epoch
             )
 
         elif optimizer_config["schedule_type"] == "multistep":
             # Multi step
-            steps = [num_iters_per_epoch * step for step in optimizer_config["schedule_steps"]]
+            steps = [
+                num_iters_per_epoch * step
+                for step in optimizer_config["schedule_steps"]
+            ]
             scheduler = LinearWarmupMultiStepLR(
                 optimizer,
                 warmup_steps,
                 steps,
                 gamma=optimizer_config["schedule_gamma"],
-                last_epoch=last_epoch
+                last_epoch=last_epoch,
             )
         else:
             raise TypeError("Unsupported scheduler!")
@@ -169,19 +170,17 @@ def make_scheduler(
         if optimizer_config["schedule_type"] == "cosine":
             # step per iteration
             scheduler = optim.lr_scheduler.CosineAnnealingLR(
-                optimizer,
-                max_steps,
-                last_epoch=last_epoch
+                optimizer, max_steps, last_epoch=last_epoch
             )
 
         elif optimizer_config["schedule_type"] == "multistep":
             # step every some epochs
-            steps = [num_iters_per_epoch * step for step in optimizer_config["schedule_steps"]]
+            steps = [
+                num_iters_per_epoch * step
+                for step in optimizer_config["schedule_steps"]
+            ]
             scheduler = optim.lr_scheduler.MultiStepLR(
-                optimizer,
-                steps,
-                gamma=schedule_config["gamma"],
-                last_epoch=last_epoch
+                optimizer, steps, gamma=schedule_config["gamma"], last_epoch=last_epoch
             )
         else:
             raise TypeError("Unsupported scheduler!")
@@ -193,6 +192,7 @@ class AverageMeter(object):
     """Computes and stores the average and current value.
     Used to compute dataset stats from mini-batches
     """
+
     def __init__(self):
         self.initialized = False
         self.val = None
@@ -233,13 +233,17 @@ class ModelEma(torch.nn.Module):
 
     def _update(self, model, update_fn):
         with torch.no_grad():
-            for ema_v, model_v in zip(self.module.state_dict().values(), model.state_dict().values()):
+            for ema_v, model_v in zip(
+                self.module.state_dict().values(), model.state_dict().values()
+            ):
                 if self.device is not None:
                     model_v = model_v.to(device=self.device)
                 ema_v.copy_(update_fn(ema_v, model_v))
 
     def update(self, model):
-        self._update(model, update_fn=lambda e, m: self.decay * e + (1. - self.decay) * m)
+        self._update(
+            model, update_fn=lambda e, m: self.decay * e + (1.0 - self.decay) * m
+        )
 
     def set(self, model):
         self._update(model, update_fn=lambda e, m: m)
@@ -252,10 +256,10 @@ def train_one_epoch(
     optimizer,
     scheduler,
     curr_epoch,
-    model_ema = None,
-    clip_grad_l2norm = -1,
-    tb_writer = None,
-    print_freq = 20
+    model_ema=None,
+    clip_grad_l2norm=-1,
+    tb_writer=None,
+    print_freq=20,
 ):
     """Training the model for one epoch"""
     # set up meters
@@ -274,13 +278,10 @@ def train_one_epoch(
         optimizer.zero_grad(set_to_none=True)
         # forward / backward the model
         losses = model(video_list)
-        losses['final_loss'].backward()
+        losses["final_loss"].backward()
         # gradient cliping (to stabilize training if necessary)
         if clip_grad_l2norm > 0.0:
-            torch.nn.utils.clip_grad_norm_(
-                model.parameters(),
-                clip_grad_l2norm
-            )
+            torch.nn.utils.clip_grad_norm_(model.parameters(), clip_grad_l2norm)
         # step optimizer / scheduler
         optimizer.step()
         scheduler.step()
@@ -308,47 +309,32 @@ def train_one_epoch(
             global_step = curr_epoch * num_iters + iter_idx
             if tb_writer is not None:
                 # learning rate (after stepping)
-                tb_writer.add_scalar(
-                    'train/learning_rate',
-                    lr,
-                    global_step
-                )
+                tb_writer.add_scalar("train/learning_rate", lr, global_step)
                 # all losses
                 tag_dict = {}
                 for key, value in losses_tracker.items():
                     if key != "final_loss":
                         tag_dict[key] = value.val
-                tb_writer.add_scalars(
-                    'train/all_losses',
-                    tag_dict,
-                    global_step
-                )
+                tb_writer.add_scalars("train/all_losses", tag_dict, global_step)
                 # final loss
                 tb_writer.add_scalar(
-                    'train/final_loss',
-                    losses_tracker['final_loss'].val,
-                    global_step
+                    "train/final_loss", losses_tracker["final_loss"].val, global_step
                 )
 
             # print to terminal
-            block1 = 'Epoch: [{:03d}][{:05d}/{:05d}]'.format(
+            block1 = "Epoch: [{:03d}][{:05d}/{:05d}]".format(
                 curr_epoch, iter_idx, num_iters
             )
-            block2 = 'Time {:.2f} ({:.2f})'.format(
-                batch_time.val, batch_time.avg
+            block2 = "Time {:.2f} ({:.2f})".format(batch_time.val, batch_time.avg)
+            block3 = "Loss {:.2f} ({:.2f})\n".format(
+                losses_tracker["final_loss"].val, losses_tracker["final_loss"].avg
             )
-            block3 = 'Loss {:.2f} ({:.2f})\n'.format(
-                losses_tracker['final_loss'].val,
-                losses_tracker['final_loss'].avg
-            )
-            block4 = ''
+            block4 = ""
             for key, value in losses_tracker.items():
                 if key != "final_loss":
-                    block4  += '\t{:s} {:.2f} ({:.2f})'.format(
-                        key, value.val, value.avg
-                    )
+                    block4 += "\t{:s} {:.2f} ({:.2f})".format(key, value.val, value.avg)
 
-            print('\t'.join([block1, block2, block3, block4]))
+            print("\t".join([block1, block2, block3, block4]))
 
     # finish up and print
     lr = scheduler.get_last_lr()[0]
@@ -360,11 +346,11 @@ def valid_one_epoch(
     val_loader,
     model,
     curr_epoch,
-    ext_score_file = None,
-    evaluator = None,
-    output_file = None,
-    tb_writer = None,
-    print_freq = 20
+    ext_score_file=None,
+    evaluator=None,
+    output_file=None,
+    tb_writer=None,
+    print_freq=20,
 ):
     """Test the model on the validation set"""
     # either evaluate the results or save the results
@@ -375,13 +361,7 @@ def valid_one_epoch(
     # switch to evaluate mode
     model.eval()
     # dict for results (for our evaluation code)
-    results = {
-        'video-id': [],
-        't-start' : [],
-        't-end': [],
-        'label': [],
-        'score': []
-    }
+    results = {"video-id": [], "t-start": [], "t-end": [], "label": [], "score": []}
 
     # loop over validation set
     start = time.time()
@@ -393,15 +373,15 @@ def valid_one_epoch(
             # unpack the results into ANet format
             num_vids = len(output)
             for vid_idx in range(num_vids):
-                if output[vid_idx]['segments'].shape[0] > 0:
-                    results['video-id'].extend(
-                        [output[vid_idx]['video_id']] *
-                        output[vid_idx]['segments'].shape[0]
+                if output[vid_idx]["segments"].shape[0] > 0:
+                    results["video-id"].extend(
+                        [output[vid_idx]["video_id"]]
+                        * output[vid_idx]["segments"].shape[0]
                     )
-                    results['t-start'].append(output[vid_idx]['segments'][:, 0])
-                    results['t-end'].append(output[vid_idx]['segments'][:, 1])
-                    results['label'].append(output[vid_idx]['labels'])
-                    results['score'].append(output[vid_idx]['scores'])
+                    results["t-start"].append(output[vid_idx]["segments"][:, 0])
+                    results["t-end"].append(output[vid_idx]["segments"][:, 1])
+                    results["label"].append(output[vid_idx]["labels"])
+                    results["score"].append(output[vid_idx]["scores"])
 
         # printing
         if (iter_idx != 0) and iter_idx % (print_freq) == 0:
@@ -411,15 +391,18 @@ def valid_one_epoch(
             start = time.time()
 
             # print timing
-            print('Test: [{0:05d}/{1:05d}]\t'
-                  'Time {batch_time.val:.2f} ({batch_time.avg:.2f})'.format(
-                  iter_idx, len(val_loader), batch_time=batch_time))
+            print(
+                "Test: [{0:05d}/{1:05d}]\t"
+                "Time {batch_time.val:.2f} ({batch_time.avg:.2f})".format(
+                    iter_idx, len(val_loader), batch_time=batch_time
+                )
+            )
 
     # gather all stats and evaluate
-    results['t-start'] = torch.cat(results['t-start']).numpy()
-    results['t-end'] = torch.cat(results['t-end']).numpy()
-    results['label'] = torch.cat(results['label']).numpy()
-    results['score'] = torch.cat(results['score']).numpy()
+    results["t-start"] = torch.cat(results["t-start"]).numpy()
+    results["t-end"] = torch.cat(results["t-end"]).numpy()
+    results["label"] = torch.cat(results["label"]).numpy()
+    results["score"] = torch.cat(results["score"]).numpy()
 
     if evaluator is not None:
         if ext_score_file is not None and isinstance(ext_score_file, str):
@@ -434,6 +417,6 @@ def valid_one_epoch(
 
     # log mAP to tb_writer
     if tb_writer is not None:
-        tb_writer.add_scalar('validation/mAP', mAP, curr_epoch)
+        tb_writer.add_scalar("validation/mAP", mAP, curr_epoch)
 
     return mAP
